bbc5fd0bb4be907d788893f3b96ed4dbab32885f
Elias-Olofsson <54439741+Elias-Olofsson@users.noreply.github.com>
2020-08-26 09:17:23 +0200

    Add files via upload

---
+++ arv/Shape.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arv
+{
+    class Shape
+    {
+        protected int height;
+        protected int width;
+
+        public Shape(int height, int width)
+        {
+            this.height = height;
+            this.width = width;
+        }
+    }
+}
---
+++ arv/Triangle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arv
+{
+    class Triangle : Shape
+    {
+        public Triangle(int height, int witdh):base(height, witdh)
+        {
+
+        }
+    }
+}
