22a2522ee12c47d85f7e8eaaaf03af36da49bb06
Elias-Olofsson <54439741+Elias-Olofsson@users.noreply.github.com>
2020-09-01 11:04:00 +0200

    Jag har fixat rektangel och börjat med main.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Börja med att skriva höjd.");
+            int height = int.Parse(Console.ReadLine());
+            Console.WriteLine("Skriv bredd.");
+            int bredd = int.Parse(Console.ReadLine());
+            Console.WriteLine("");
+            
+            Triangle tri = new Triangle(height, 0);
+        }
+    }
+}
---
+++ Triangle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arv
+{
+    class Triangle : Shape
+    {
+        public Triangle(int height, int witdh):base(height, witdh)
+        {
+           
+        }
+
+        public int Area()
+        {
+            return 
+                (height * width) /2;
+        }
+
+        public int Circumferance()
+        {
+            return
+                width * 3;
+        }
+    }
+}
